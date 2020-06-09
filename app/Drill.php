8e6cb516801626a779a21822865cd126d383a3b3
ojou <musiclear05@gmail.com>
2020-06-09 17:33:26 +0900

    view 修正

--- app/Drill.php
+++ app/Drill.php
@@ -6,6 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Drill extends Model
 {
-    protected $table = 'my_drills';
     protected $fillable = ['title', 'category_name', 'problem0', 'problem1', 'problem2', 'problem3', 'problem4', 'problem5', 'problem6', 'problem7', 'problem8', 'problem9'];
 }
