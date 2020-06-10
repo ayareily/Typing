8c10731ea2d3d1317360af919f9d166e097e87b3
ojou <musiclear05@gmail.com>
2020-06-10 12:07:47 +0900

    laravel mixの設定

--- resources/views/drills/show.blade.php
+++ resources/views/drills/show.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
     <div id="app">
-        <!-- デフォルトだとこの中ではvue.jsが有効 -->
-        <!-- example-component はLaravelに入っているサンプルのコンポーネント -->
         <example-component title="{{ __('Practice').'「'.$drill->title.'」' }}" :drill="{{$drill}}"></example-component>
     </div>
 @endsection
